2563c52122295e8c7a989c50d8b2b0583e64edbe
Daniel Clough <danielleeclough86@hotmail.co.uk>
2021-11-21 12:53:08 +0000

    Adding initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//TO-DO LIST.
+
+const App = () => {
+ //STATE   
+ const [listItem, setListItem] = useState([])
+ const [newItem, SetItem] = useState("") 
+ 
+ //INPUT
+const handleChange = (event) => {
+   SetItem(event.target.value);
+}
+//ADD BUTTON
+const handleAdd = () => {
+    const updatedList = listItem.concat([newItem])
+    setListItem(updatedList)
+    
+}
+//REMOVE HANDLER
+const removeHandler = index => {
+    let removeItem = [...listItem]
+    removeItem.splice(index, 1)
+    setListItem(removeItem)
+}
+//WHAT IS BEING RENDERED ON SCREEN
+return(
+<div id="appContainer">
+<div id="navBar">
+<h1 className="navText">dOer</h1>
+<h2 className="navText">dO more. dO dOer!</h2>
+</div>
+
+<div id="display">
+<div id="listContainer">   
+{listItem.map((listItem, index) => {
+    return(
+        <div id="listInner" key={index}>
+        <p className="doText"> #{index + 1} dO.</p>
+        <h2 className="listItem">{listItem}</h2>
+        <button className="button" onClick={() => removeHandler (index)}>dOne</button>
+        </div>)
+ })}
+ </div>
+
+<div id="right">
+<div id="newItemContainer">
+ <h1 id="doText">new dO. new yOu</h1>
+ <p>Add your dO below to get started.</p>
+ <input id="input" type="text" placeholder="add a dO..."  value={newItem} onChange={handleChange} />
+ <button className="button2" type="button" onClick={handleAdd}>dO it!</button>
+</div>
+<div id="message"></div>
+
+</div>
+
+</div>
+
+</div>
+)
 }
 
-export default App;
+export default App
+
+
+
